0f725d971d7126d9366c7d1c24f571cf237756c8
sgrimee <sgrimee@gmail.com.me>
2024-03-15 20:57:17 +0100

    separate linux from other systems totally

--- flake.nix
+++ flake.nix
@@ -9,117 +9,63 @@
     };
   };
 
-  outputs = {
-    self,
-    nixpkgs,
-    flake-utils,
-    ...
-  }:
+  outputs =
+    { self
+    , nixpkgs
+    , flake-utils
+    , ...
+    }:
     flake-utils.lib.eachDefaultSystem (
-      system: let
+      system:
+      let
         pkgs = import nixpkgs {
           inherit system;
-          overlays = [
-          ];
+          overlays = [ ];
           config.allowUnfree = true;
           config.cudaSupport = true;
         };
-      in {
+      in
+      {
         devShell =
-          (pkgs.buildFHSUserEnv {
+          if pkgs.stdenv.isLinux
+          then (pkgs.buildFHSUserEnv {
             name = "cuda-env";
-            targetPkgs = pkgs: (with pkgs; [
-              cudatoolkit
-              linuxPackages.nvidia_x11
-              ollama
-              python312
-              python312Packages.pip
-              python312Packages.virtualenv
-            ]);
-            multiPkgs = pkgs: with pkgs; [zlib];
+            targetPkgs = pkgs:
+              with pkgs;
+              [
+                ollama
+                python312
+                python312Packages.pip
+                python312Packages.virtualenv
+                cudaPackages.cudatoolkit
+                cudaPackages.cudnn
+                linuxPackages.nvidia_x11
+              ];
             runScript = "zsh";
             profile = ''
-              DIRENV_DISABLE=avoid-recursion;
-              CUDA_PATH=${pkgs.cudatoolkit}
-              # LD_LIBRARY_PATH=${pkgs.linuxPackages.nvidia_x11}/lib
-              EXTRA_LDFLAGS="-L/lib -L${pkgs.linuxPackages.nvidia_x11}/lib"
-              EXTRA_CCFLAGS="-I/usr/include"
+              CUDA_PATH=${pkgs.cudaPackages.cudatoolkit}
             '';
-          })
-          .env;
-
-        # devShell = pkgs.mkShell.override {} {
-        #   # build environment
-        #   nativeBuildInputs = with pkgs;
-        #     [
-        #       # clang
-        #       # openssl.dev
-        #       # pkg-config
-        #     ]
-        #     ++ lib.optionals pkgs.stdenv.isLinux
-        #     [
-        #       cudaPackages.cudatoolkit
-        #       cudaPackages.cudnn
-        #       linuxPackages.nvidia_x11
-        #     ];
-
-        #   # runtime environment
-        #   buildInputs = with pkgs;
-        #     [
-        #       cudaPackages.cudatoolkit
-        #       cudaPackages.cudnn
-        #       linuxPackages.nvidia_x11
-        #       ollama
-        #       python312
-        #       python312Packages.pip
-        #       python312Packages.virtualenv
-        #     ]
-        #     ++ lib.optionals pkgs.stdenv.isDarwin [
-        #       pkgs.libiconv
-        #       pkgs.darwin.apple_sdk.frameworks.Metal
-        #       pkgs.darwin.apple_sdk.frameworks.MetalPerformanceShaders
-        #       pkgs.darwin.apple_sdk.frameworks.Security
-        #       pkgs.darwin.apple_sdk.frameworks.CoreServices
-        #       pkgs.darwin.apple_sdk.frameworks.SystemConfiguration
-        #     ];
-
-        # shellHook =
-        #   if pkgs.stdenv.isLinux
-        #   then ''
-        #     # LD_LIBRARY_PATH="${pkgs.linuxPackages.nvidia_x11}/lib"
-        #     # CUDA_ROOT="${pkgs.cudaPackages.cudatoolkit}"
-        #     CUDA_PATH="${pkgs.cudaPackages.cudatoolkit}"
-        #     CUDN_LIB="${pkgs.cudaPackages.cudnn}"
-        #     EXTRA_LDFLAGS="-L/lib -L${pkgs.linuxPackages.nvidia_x11}/lib"
-        #     EXTRA_CCFLAGS="-I/usr/include"
-        #   ''
-        #   else '''';
-
-        # CUDA_ROOT =
-        #   if pkgs.stdenv.isLinux
-        #   then v"${pkgs.cudaPackages.cudatoolkit}"
-        #   else "";
-        # CUDA_PATH =
-        #   if pkgs.stdenv.isLinux
-        #   then "${pkgs.cudaPackages.cudatoolkit}"
-        #   else "";
-        # CUDNN_LIB =
-        #   if pkgs.stdenv.isLinux
-        #   then "${pkgs.cudaPackages.cudnn}"
-        #   else "";
-
-        # EXTRA_LDFLAGS =
-        #   if pkgs.stdenv.isLinux
-        #   then "-L/lib -L${pkgs.linuxPackages.nvidia_x11}/lib"
-        #   else "";
-
-        # EXTRA_CCFLAGS = "-I/usr/include";
-        # };
-
-        # defaultPackage = pkgs.mkRustPackage {
-        #   cargoSha256 = "46652094fc5f1f00761992c876b6712052edd15eefd93b2e309833a30af94a95";
-        #   src = ./.;
-        # };
+          }).env
+          else
+            pkgs.mkShell.override { } {
+              # runtime environment
+              buildInputs = with pkgs;
+                [
+                  ollama
+                  python312
+                  python312Packages.pip
+                  python312Packages.virtualenv
+                ];
+              # ++ lib.optionals pkgs.stdenv.isDarwin [
+              #   pkgs.libiconv
+              #   pkgs.darwin.apple_sdk.frameworks.Metal
+              #   pkgs.darwin.apple_sdk.frameworks.MetalPerformanceShaders
+              #   pkgs.darwin.apple_sdk.frameworks.Security
+              #   pkgs.darwin.apple_sdk.frameworks.CoreServices
+              #   pkgs.darwin.apple_sdk.frameworks.SystemConfiguration
+              # ];
+            };
       }
     );
 }
+
